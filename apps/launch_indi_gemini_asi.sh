f569c8d5c3fb5adcf928428821f8c80502a89144
gnthibault <gnthibault@gmail.com>
2018-10-15 19:39:36 +0200

    Working on implementing a real setup

--- apps/launch_indi_gemini_asi.sh
+++ apps/launch_indi_gemini_asi.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # run ssh -L 8080:192.168.1.2:80 192.168.0.29; and the connect http://localhost:8080/ to access gemini interface, default conf is udp 11110 or tcp 4030
-indiserver indi_lx200gemini
+indiserver indi_lx200gemini indi_asi_ccd
