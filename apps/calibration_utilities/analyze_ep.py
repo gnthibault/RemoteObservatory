5c6b6f9e4db98136b225e8460f1aba537af97b90
gnthibault <gnthibault@gmail.com>
2018-10-31 14:28:16 +0100

    New version of periodic error analyzer with resampling and lag correction, untested, need to debug

--- apps/calibration_utilities/analyze_ep.py
+++ apps/calibration_utilities/analyze_ep.py
@@ -3,26 +3,28 @@ import matplotlib.pyplot as plt
 import numpy as np
 import os
 import pandas as pd
+import scipy.interpolate
+import  scipy.signal 
 
 # time stuff
 from dateutil.parser import parse
 from datetime import datetime, date, time, timedelta
 
 
-def plot_data(x, y, ax, ax2, ax3, filename):
+def plot_data(data, basename, timestep, ax, ax2, ax3, filename):
     # plot raw data
-    ax.plot(x, y, label=filename)
+    ax.plot(data['x'], data[basename], label=filename)
 
-    # plot a version where first order polynomial has been removed
-    pol = np.polyfit(x, y, deg=1)
-    y2 = y-np.sum([pol[pol.size-1-i]*(x**i) for i in range(pol.size)], axis=0)
-    ax2.plot(x, y2, label=filename)
-    ax2.set_ylim(np.percentile(y2,2)*2, np.percentile(y2,98)*2)
+    # plot a version where first order polynomial has been removed, and content
+    # has been resampled and aligned
+    x = data['x_resampled_lag']
+    y = data[basename+'_highpass_resampled']
+    ax2.plot(x, y, label=filename)
+    ax2.set_ylim(np.percentile(y,2)*2, np.percentile(y,98)*2)
 
     # plot fourier transform of the error in original format
-    fourier = np.fft.rfft(y2)
-    n = y2.size
-    timestep = np.mean(np.roll(x,-1)[:-1]-x[:-1])
+    fourier = np.fft.rfft(y)
+    n = y.size
     freq = np.fft.rfftfreq(n, d=timestep)
     # assume max period of 10 minutes
     period=(1/freq)[::-1]
@@ -71,6 +73,8 @@ def analyze_ep(directory):
     dec_ax3.set_xlabel('Time Period (in sec)')
     dec_ax3.set_ylabel('Magnitude')
 
+    # first, build the dictionary of data
+    datas = {}
     for filename in [f for f in os.listdir(directory) if f.endswith('.csv')]:
         # load data
         full_filename=os.path.join(directory, filename)
@@ -80,8 +84,66 @@ def analyze_ep(directory):
         #x=(x-x.min()).dt.total_seconds()
         ra_y=df[' RA Error (arcsec)']
         dec_y=df[' DE Error (arcsec)']
-        plot_data(x, ra_y, ra_ax, ra_ax2, ra_ax3, filename)
-        plot_data(x, dec_y, dec_ax, dec_ax2, dec_ax3, filename)
+        #Add new entry
+        datas[filename]={'x':x, 'ra_y':ra_y, 'dec_y':dec_y}
+        #plot_data(x, ra_y, ra_ax, ra_ax2, ra_ax3, filename)
+        #plot_data(x, dec_y, dec_ax, dec_ax2, dec_ax3, filename)
+
+    # save a version where first order polynomial has been removed
+    for key, data in datas.items():
+        x = data['x']
+        for dataname in ['y_ra', 'y_dec']:
+            y = data[dataname]
+            pol = np.polyfit(x, y, deg=1)
+            y2 = y-np.sum([pol[pol.size-1-i]*(x**i) for i in range(pol.size)],
+                          axis=0)
+            data[dataname+'_highpass'] = y2
+
+    # Now try to find a common time step. Best policy is to select the timestep
+    # that offers the best spectral resolution given some min/max constraints
+    timesteps = []
+    weights = []
+    for key, data in datas.items():
+        x = data['x']
+        timesteps.append(np.mean(np.roll(x,-1)[:-1]-x[:-1]))
+        weights.append(len(x))
+    timestep = (np.array(timesteps)*np.array(weight)).sum()/sum(weights)
+
+    # resample all signals exactly on the same regular grid
+    for key, data in datas.items():
+        x = data['x']
+        new_x = np.arange(np.round((x.max()-x.min())/timestep)+1)*timestep
+        for dataname in ['y_ra_highpass', 'y_dec_highpass']:
+            y = data[dataname]
+            interp = scipy.interpolate.interp1d(x, y, kind='cubic',
+                                                fill_value='extrapolate')
+            data[dataname+'_resampled'] = interp(new_x)
+        data['x_resampled'] = new_x
+
+    ###############################################################
+    # Now we would like to register signals on y_ra reference     #
+    ###############################################################
+    # first, we choose the longest time serie as a reference
+    longest_time_serie = max(datas, key=lambda k: len(datas[k]['x']))
+    dataname = 'y_ra_highpass_resampled'
+    ref_y = datas[longest_time_serie][dataname]
+
+    # Now, find the lag with respect to reference(fp number)
+    for key, data in datas.items():
+        y = data[dataname]
+        lag = np.argmax(scipy.signal.correlate(ref_y, y))
+        #Now that lag is given, we may pad with nans ?
+        #c_sig = np.roll(b, shift=int(np.ceil(lag)))
+        data['lag'] = lag
+        data['x_resampled_lag'] = data['x_resampled']+lag
+
+    ###########################
+    # Now plot everything     #
+    ###########################
+    for filename, data in datas.items():
+        #Add new entry
+        plot_data(data, 'y_ra', timestep, ra_ax, ra_ax2, ra_ax3, filename)
+        plot_data(data, 'y_dec', timestep, dec_ax, dec_ax2, dec_ax3, filename)
 
 
     # Set ticks/cosmetic stuff
@@ -93,12 +155,14 @@ def analyze_ep(directory):
         ax.legend(fontsize=legend_fontsize)
     plt.show()
 
+
+
 if __name__ == '__main__':
 
     # Get the command line option
     parser = argparse.ArgumentParser(
         description="Make a plot periodic error for all csv data of the directory")
-    parser.add_argument('--directory', default='.',
+    parser.add_argument('--directory', #mandatory=True,
         help="directory containing csv")
     args = parser.parse_args()
     analyze_ep(directory=args.directory)
