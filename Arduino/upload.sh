607668eb5d87cd7dae9b84c43cc053994c201c3b
rock <rock@localhost.localdomain>
2019-06-10 19:42:15 +0000

    Doing stuff with the controller

--- Arduino/upload.sh
+++ Arduino/upload.sh
@@ -2,10 +2,10 @@
 
 #APP=Controller
 APP=Firmata
-
 in="./build/$APP/$APP"
 out="./build/$APP/$APP.hex"
-tty=$(ls /dev/ttyACM*)
+#tty=$(ls /dev/ttyACM*)
+tty=/dev/arduino
 avr-objcopy -O ihex -R .eeprom $in $out
-avrdude -V -carduino -patmega328p -b115200 -P $tty -D -Uflash:w:$out:i \
+avrdude -V -carduino -pATmega328P -b57600 -P $tty -D -Uflash:w:$out:i \
   -C ./avrdude.conf
