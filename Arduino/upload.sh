423363ec1565281517ffca05f89b8787e1776e36
gnthibault <gnthibault@gmail.com>
2019-06-20 08:34:02 +0200

    Working on the embedded aspect with arduini

--- Arduino/upload.sh
+++ Arduino/upload.sh
@@ -7,5 +7,5 @@ in="./build/$APP/$APP"
 out="./build/$APP/$APP.hex"
 tty=$(ls /dev/ttyACM*)
 avr-objcopy -O ihex -R .eeprom $in $out
-avrdude -V -carduino -patmega328p -b115200 -P $tty -D -Uflash:w:$out:i \
+avrdude -V -carduino -pm328p -b115200 -P $tty -D -Uflash:w:$out:i \
   -C ./avrdude.conf
