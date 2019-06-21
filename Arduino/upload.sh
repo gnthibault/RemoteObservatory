bc18b189f489882aee75d4ca476ca38ac346a84f
rock <rock@localhost.localdomain>
2019-06-21 18:01:30 +0000

    Working with rockpro64

--- Arduino/upload.sh
+++ Arduino/upload.sh
@@ -4,8 +4,18 @@
 APP=Firmata
 in="./build/$APP/$APP"
 out="./build/$APP/$APP.hex"
-#tty=$(ls /dev/ttyACM*)
-tty=/dev/arduino
+
+# If arduino uno, think about sudo adduser $USER dialout
+tty=$(ls /dev/ttyACM*)
+#tty=/dev/arduino
+device=m328p
+baud=115200
+
+# If arduino nano
+#tty=/dev/arduino
+#device=m328
+#baud=57600
+
 avr-objcopy -O ihex -R .eeprom $in $out
-avrdude -V -carduino -pATmega328P -b57600 -P $tty -D -Uflash:w:$out:i \
+avrdude -V -carduino -p$device -b$baud -P $tty -D -Uflash:w:$out:i \
   -C ./avrdude.conf
