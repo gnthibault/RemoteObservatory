6f53d5e56ee93abd22892e645f6a6b73030850a1
gnthibault <gnthibault@gmail.com>
2019-06-21 20:25:53 +0200

    Updated for arduino nano

--- Arduino/upload.sh
+++ Arduino/upload.sh
@@ -7,15 +7,16 @@ in="./build/$APP/$APP"
 out="./build/$APP/$APP.hex"
 
 # If arduino uno, think about sudo adduser $USER dialout
-tty=$(ls /dev/ttyACM*)
+#tty=$(ls /dev/ttyACM*)
 #tty=/dev/arduino
-device=m328p
-baud=115200
+#device=m328p
+#baud=115200
 
 # If arduino nano
+tty=$(ls /dev/ttyUSB*)
 #tty=/dev/arduino
-#device=m328
-#baud=57600
+device=m328p
+baud=57600
 
 avr-objcopy -O ihex -R .eeprom $in $out
 avrdude -V -carduino -p$device -b$baud -P $tty -D -Uflash:w:$out:i \
