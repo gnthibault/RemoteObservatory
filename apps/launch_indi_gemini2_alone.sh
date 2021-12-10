d1d03d5005a11ccf7268d18fdcc17ff696e00c09
gnthibault <gnthibault@gmail.com>
2021-12-10 20:23:01 +0100

    Updated info for gemini 2, only udp is working

--- apps/launch_indi_gemini2_alone.sh
+++ apps/launch_indi_gemini2_alone.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 # run ssh -L 8080:192.168.1.2:80 192.168.0.29; and the connect http://localhost:8080/ to access gemini interface, default conf is udp 11110 or tcp 4030 login is admin with no passwd 
+# edit, only udp conf works
 indiserver indi_lx200gemini
