29c9e3e10d5975abc3af2c2e12e54cc5f0b661c5
gnthibault <gnthibault@gmail.com>
2021-12-10 20:01:15 +0100

    Updated info for gemini 2 web interface

--- apps/launch_indi_gemini_canon.sh
+++ apps/launch_indi_gemini_canon.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-# run ssh -L 8080:192.168.1.2:80 192.168.0.29; and the connect http://localhost:8080/ to access gemini interface, default conf is udp 11110 or tcp 4030
+# run ssh -L 8080:192.168.1.2:80 192.168.0.29; and the connect http://localhost:8080/ to access gemini interface, default conf is udp 11110 or tcp 4030 login is admin with no passwd 
 # you can also play with canon dslr / do a check with rm -rf ./test.cr2 && gphoto2 --capture-image-and-download --filename "test.cr2"
 indiserver indi_lx200gemini indi_canon_ccd
