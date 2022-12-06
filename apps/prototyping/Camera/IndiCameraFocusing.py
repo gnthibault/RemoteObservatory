66d9a1bc6b085708ddeab8a14f2c8706c7903aaa
gnthibault <gnthibault@gmail.com>
2022-12-06 08:26:19 +0100

    Meaningful values for autofocus simulator

--- apps/prototyping/Camera/IndiCameraFocusing.py
+++ apps/prototyping/Camera/IndiCameraFocusing.py
@@ -10,7 +10,8 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 # Local stuff : Camera
-from Camera.IndiCamera import IndiCamera
+from Camera.IndiAbstractCameraSimulator import IndiAbstractCameraSimulator
+from Service.NTPTimeService import HostTimeService
 
 # For this t
 if __name__ == '__main__':
@@ -20,23 +21,43 @@ if __name__ == '__main__':
 
     config = dict(
         camera_name='CCD Simulator',
+        SIMULATOR_SETTINGS=dict(
+            SIM_XRES=1000,
+            SIM_YRES=1000,
+            SIM_XSIZE=5.2,
+            SIM_YSIZE=5.2,
+            SIM_MAXVAL=65000,
+            SIM_SATURATION=1,
+            SIM_LIMITINGMAG=17,
+            SIM_NOISE=0.5,
+            SIM_SKYGLOW=19.5,
+            SIM_OAGOFFSET=0,
+            SIM_POLAR=0,
+            SIM_POLARDRIFT=0,
+            SIM_ROTATION=0,
+            SIM_PEPERIOD=0,
+            SIM_PEMAX=0,
+            SIM_TIME_FACTOR=1),
+        SCOPE_INFO=dict(
+            FOCAL_LENGTH=800,
+            APERTURE=200),
+        pointing_seconds=30,
         autofocus_seconds=5,
-        pointing_seconds=10,
-        autofocus_size=500,
+        autofocus_size=650,
         autofocus_merit_function="half_flux_radius",
         focuser=dict(
             module="IndiFocuser",
             focuser_name="Focuser Simulator",
             port="/dev/ttyUSB0",
             focus_range=dict(
-                min=1,
-                max=100000),
+                min=25000,
+                max=50000),
             autofocus_step=dict(
-                coarse=10000,
-                fine=500),
+                coarse=2500,
+                fine=1000),
             autofocus_range=dict(
-                coarse=100000,
-                fine=20000),
+                coarse=25000,
+                fine=10000),
             indi_client=dict(
                 indi_host="localhost",
                 indi_port="7624")
@@ -47,7 +68,7 @@ if __name__ == '__main__':
         )
 
     # test indi virtual camera class
-    cam = IndiCamera(config=config, connect_on_create=True)
+    cam = IndiAbstractCameraSimulator(serv_time=HostTimeService(), config=config, connect_on_create=True)
     cam.prepare_shoot()
 
     def get_thumb(cam):
