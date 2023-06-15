a15d847c116249d1eecee0dd7a22d5df238c84fe
gnthibault <gnthibault@gmail.com>
2023-06-15 08:08:18 +0200

    TBD

--- Camera/IndiASICamera.py
+++ Camera/IndiASICamera.py
@@ -2,6 +2,7 @@
 
 # Local stuff
 from Camera.IndiAbstractCamera import IndiAbstractCamera
+from Camera.IndiCamera import IndiCamera
 
 class IndiASICamera(IndiAbstractCamera):
     def __init__(self, serv_time, config=None,
@@ -13,15 +14,18 @@ class IndiASICamera(IndiAbstractCamera):
             config=config,
             connect_on_create=connect_on_create)
 
-    # Always use maximum dynamic
-    dyn = self.get_dynamic()
-    max_dyn = self.get_maximum_dynamic()
-    if dyn != max_dyn:
-        self.logger.warning(f"Camera {self.name} using format {self.get_current_format()} with dynamic {dyn} although it "
-                            f"is capable of {max_dyn}. Trying to set maximum bit depth")
-        self.set_switch("CCD_VIDEO_FORMAT", ["ASI_IMG_RAW16"])
-        self.logger.info(f"Now camera {self.name} has format {self.get_current_format()} allowing for dynamic "
-                         f"{self.get_dynamic()}")
+    def prepare_shoot(self):
+        # Call parent method
+        IndiCamera.prepare_shoot(self)
+        # Always use maximum dynamic
+        dyn = self.get_dynamic()
+        max_dyn = self.get_maximum_dynamic()
+        if dyn != max_dyn:
+            self.logger.warning(f"Camera {self.name} using format {self.get_current_format()} with dynamic {dyn} although it "
+                                f"is capable of {max_dyn}. Trying to set maximum bit depth")
+            self.set_switch("CCD_VIDEO_FORMAT", ["ASI_IMG_RAW16"])
+            self.logger.info(f"Now camera {self.name} has format {self.get_current_format()} allowing for dynamic "
+                             f"{self.get_dynamic()}")
 
     def get_current_format(self):
         return [key for key, val in self.get_switch('CCD_VIDEO_FORMAT').items() if val == "On"]
---
+++ apps/prototyping/Camera/IndiASICameraTest.py
@@ -0,0 +1,88 @@
+# Basic stuff
+
+# Viz stuff
+import matplotlib.pyplot as plt
+from skimage import img_as_float
+from skimage import exposure
+
+# Local stuff : IndiClient
+from helper.IndiClient import IndiClient
+
+# Local stuff : Camera
+from Camera.IndiASICamera import IndiASICamera
+
+
+if __name__ == '__main__':
+    # test indi client
+    # config = dict(
+    # camera_name='Altair AA183MPRO',
+    # autofocus_seconds=5,
+    # pointing_seconds=30,
+    # autofocus_roi_size=500,
+    # indi_client=dict(
+    #     indi_host="192.168.0.33",
+    #     indi_port="7624"
+    # ))
+    config = dict(
+        camera_name='ZWO CCD ASI183MM Pro',
+        pointing_seconds=30,
+        autofocus_seconds=5,
+        autofocus_roi_size=500,
+        autofocus_merit_function="half_flux_radius",
+        indi_client=dict(
+            indi_host="localhost",
+            indi_port=7624
+    ))
+
+    # test indi virtual camera class
+    cam = IndiASICamera(config=config,
+                        connect_on_create=False)
+    cam.connect()
+
+    # Play with camera configuration
+    cam.set_roi({'X': 256, 'Y': 480, 'WIDTH': 512, 'HEIGHT': 640})
+    # get_roi
+    print(f"Current camera ROI is: {cam.get_roi()}")
+    cam.set_roi({'X': 0, 'Y': 0, 'WIDTH': 1280, 'HEIGHT': 1024})
+    # get_roi
+    print(f"Current camera ROI is: {cam.get_roi()}")
+
+    #print('Setting cooling on')
+    #cam.set_cooling_on() THIS VECTOR IS EXPECTED TO BE IN BUSY STATE, NOT IDLE NOR OK, THAT's WHY THERE IS TIMEOUT
+    print(f"Current camera temperature is: {cam.get_temperature()}")
+    target_temp = -5.5
+    print(f"Now, setting temperature to: {target_temp}")
+    cam.set_temperature(target_temp)
+    print(f"Current camera temperature is: {cam.get_temperature()}")
+    target_temp = -8
+    print(f"Now, setting temperature to: {target_temp}")
+    cam.set_temperature(target_temp)
+    print(f"Current camera temperature is: {cam.get_temperature()}")
+    #cam.set_cooling_off()
+
+    # set frame type (mostly for simulation purpose
+    cam.set_frame_type('FRAME_DARK')
+    cam.set_frame_type('FRAME_FLAT')
+    cam.set_frame_type('FRAME_BIAS')
+    cam.set_frame_type('FRAME_LIGHT')
+
+    # set gain
+    print(f"gain is {cam.get_gain()}")
+    cam.set_gain(100)
+    print(f"gain is {cam.get_gain()}")
+
+    # Acquire data
+    cam.prepare_shoot()
+    cam.setExpTimeSec(10)
+    cam.shoot_async()
+    cam.synchronize_with_image_reception()
+    fits = cam.get_received_image()
+
+    # Show image
+    fig, ax = plt.subplots(1, figsize=(16, 9))
+    img = fits[0].data
+    img_eq = exposure.equalize_hist(img)
+    print_ready_img = img_as_float(img_eq)
+    print(f"Print ready has shape {print_ready_img.shape}")
+    ax.imshow(print_ready_img)
+    plt.show()
